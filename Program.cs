8728544cb2d10d2a382328717c512acd60c607d4
Nikolai Gladchenko <glad4enkonm@gmail.com>
2020-10-10 00:38:50 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace structogen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
